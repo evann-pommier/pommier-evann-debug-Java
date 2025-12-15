8ee969055023c7d8601a7842908f2f039229702c
Neylle <evannpommier64@gmail.com>
2025-12-15 11:36:42 +0100

    writer implementation

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -0,0 +1,30 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class WriteSymptomDataToFile implements ISymptomWriter {
+	private final String filepath;
+	
+	public WriteSymptomDataToFile(String filepath) {
+        this.filepath = "result.out";
+    }
+	
+	@Override
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		if (filepath == null || symptoms == null) return;
+
+        try (FileWriter writer = new FileWriter(filepath)) {
+            // TreeMap to ensure alphabetical order
+            Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+            for (Map.Entry<String, Integer> entry : sortedSymptoms.entrySet()) {
+                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur d'écriture  dans le fichier : " + filepath);
+            e.printStackTrace();
+        }
+    }	
+}
